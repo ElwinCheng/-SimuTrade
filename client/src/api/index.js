885a2409d1f5e3a0af5f464a67ad41c98257cb3c
Elwin Cheng <67119805+ElwinCheng@users.noreply.github.com>
2022-01-23 10:39:12 -0800

    changed server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:2000'})
+//const API = axios.create({ baseURL: 'http://localhost:2000'})
+const API = axios.create({ baseURL: 'http://simutrade.herokuapp.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
